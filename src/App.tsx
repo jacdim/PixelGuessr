e5c89d056eb478bc6d4cd8deeacc0baec98c9d7e
jacdim <mxlimscrimali@gmail.com>
2024-04-10 22:07:33 +1000

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Props ={
+
 }
 
+type State = {
+  
+}
+
+const getRandomInRange = (min: number, max: number) => {
+  return Math.random() * (max - min) + min;
+}
+
+
+
+class App extends React.Component<Props, State> {
+  private pixelLocation: [number, number]
+
+
+  constructor(p) {
+    super(p)
+    this.pixelLocation = [getRandomInRange(0,window.innerWidth), getRandomInRange(0,window.innerHeight)]
+    console.log(this.pixelLocation)
+  }
+
+  render() {
+    return (
+      <div className={'text-primary bg-primary'}>
+        literally why arent you black
+      </div>
+    )
+  }
+  
+}
 export default App;
+
+
